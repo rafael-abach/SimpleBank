456b1d28adf1b3543e2367ad555adf0d9c793d97
Rafael <rafael.abacherly@gmail.com>
2023-01-19 18:19:42 -0300

    Finalizando módulo de abertura de conta

--- src/AccountOpening.java
+++ src/AccountOpening.java
@@ -1,51 +1,46 @@
+import java.util.HashMap;
 
 public class AccountOpening {
-	private String documentNumber;
-	private String name;
-//	private String birthdate;
-//	private String country;
-//	private String state;
-//	private String city;
-//	private String postalCode;
-//	private String address;
-//	private int addressNumber;
-//	private String addressComplement;
-//	private double income;
-//	private String password;
 	
-	public AccountOpening(String documentNumber,
-					      String name) {
-//						  String birthdate,
-//					      String country,
-//					      String state,
-//						  String city,
-//						  String postalCode,
-//						  String address,
-//						  int addressNumber,
-//					      String addressComplement,
-//						  double income,
-//						  String password) {
+	private String name;
+	private String documentNumber;
+	private String birthdate;
+	private HashMap<?, ?> address;
+	private double income;
+	private String password;
+	
+	public AccountOpening(String name,
+					      String documentNumber,
+						  String birthdate,
+						  HashMap<?, ?> address,
+						  double income,
+						  String password) {
 		
+		this.name =  name;
 		this.documentNumber = documentNumber;
-		this.name = name;
-//		this.birthdate = birthdate;
-//		this.country = country;
-//		this.state = state;
-//		this.city = city;
-//		this.postalCode = postalCode;
-//		this.address = address;
-//		this.addressNumber = addressNumber;
-//		this.addressComplement = addressComplement;
-//		this.income = income;
-//		this.password = password;
+		this.birthdate = birthdate;
+		this.address = address;
+		this.income = income;
+		this.password = password;
 		
 	}
-	
+
+	public String getname() {
+		return this.name;
+	}
 	public String getdocumentNumber() {
 		return this.documentNumber;
 	}
-	
-	public String getname() {
-		return this.name;
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public HashMap<?, ?> getAddress() {
+		return address;
+	}
+	public double getIncome() {
+		return income;
+	}
+	public String getPassword() {
+		return password;
 	}
 }
--- src/SimpleBank.java
+++ src/SimpleBank.java
@@ -1,8 +1,8 @@
 //package Operations;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
@@ -11,22 +11,43 @@ public class SimpleBank {
 
 	public static void main(String[] args) {
 		
+		// System.out.println("Working Directory = " + System.getProperty("user.dir") + "\n");
+		
 		try {
 			Gson gson = new Gson();
 			
-			JsonReader reader = new JsonReader(new FileReader("C:\\Users\\Rafael A\\Documents\\Projects\\SimpleBank\\src\\Operations\\operation_1.json"));
+			JsonReader reader = new JsonReader(new FileReader("./src/Operations/ops_account_opening.json"));
 			
 			AccountOpening[] clients = gson.fromJson(reader, AccountOpening[].class);
 			
-			for (int i = 0; i < clients.length; i++) {System.out.println(clients[i].getdocumentNumber() + " - " + clients[i].getname());}
+			for (int i = 0; i < clients.length; i++) {
+				System.out.println("Name: " + clients[i].getname());
+				System.out.println("Document Number: " + clients[i].getdocumentNumber());
+				System.out.println("Country: " + clients[i].getAddress().get("country"));
+				System.out.println("State: " + clients[i].getAddress().get("state"));
+				System.out.println("City: " + clients[i].getAddress().get("city"));
+				System.out.println("Postal Code: " + clients[i].getAddress().get("postalCode"));
+				System.out.println("Address: " + clients[i].getAddress().get("address"));
+				System.out.println("Address Number: " + clients[i].getAddress().get("addressNumber"));
+				System.out.println("Address Complement: " + clients[i].getAddress().get("addressComplement"));
+				System.out.println("Income ($): " + clients[i].getIncome());
+				System.out.println("Password: " + clients[i].getPassword());
+				System.out.println("Map: " + clients[i].getAddress());
+				System.out.println();
+			}
+			
+			reader.close();
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Erro generico");
+			e.printStackTrace();
 		}
-
-		
-		
 	}
 
 }
