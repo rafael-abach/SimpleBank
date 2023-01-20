f9af442a073baca46bbf0dd270764bff835c1b18
Gabriel <gaabacherly@outlook.com>
2023-01-20 17:47:49 -0300

    Refatorando (Simplificando) Leitura de Operações

--- src/SimpleBank.java
+++ src/SimpleBank.java
@@ -10,41 +10,41 @@ import java.io.FileNotFoundException;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 
+import java.util.HashMap;
+
 public class SimpleBank {
 
 	public static void main(String[] args) {
+		// System.out.println("Working Directory = " + System.getProperty("user.dir") + "\n");
 		
-		try {
-			System.out.println("Working Directory = " + System.getProperty("user.dir") + "\n");
-			
-			JsonReader reader = new JsonReader(new FileReader("./src/Operations/ops_account_opening.json"));
-			
+		try {			
 			Gson gson = new Gson();
-			AccountOpening[] clients = gson.fromJson(reader, AccountOpening[].class);
 			
-			for (int i = 0; i < clients.length; i++) {
-				System.out.println("Name: " + clients[i].getname());
-				System.out.println("Document Number: " + clients[i].getdocumentNumber());
-				System.out.println("Country: " + clients[i].getAddress().get("country"));
-				System.out.println("State: " + clients[i].getAddress().get("state"));
-				System.out.println("City: " + clients[i].getAddress().get("city"));
-				System.out.println("Postal Code: " + clients[i].getAddress().get("postalCode"));
-				System.out.println("Address: " + clients[i].getAddress().get("address"));
-				System.out.println("Address Number: " + clients[i].getAddress().get("addressNumber"));
-				System.out.println("Address Complement: " + clients[i].getAddress().get("addressComplement"));
-				System.out.println("Income ($): " + clients[i].getIncome());
-				System.out.println("Password: " + clients[i].getPassword());
+			JsonReader opsReader = new JsonReader(new FileReader("./src/Operations/operations.json"));
+			Operations[] operations = gson.fromJson(opsReader, Operations[].class);
+			
+			for (int i = 0; i < operations.length; i++) {
+				System.out.println("Type of Operation: " + operations[i].getType().toUpperCase());
+				
+				// operations[i].getDetails().forEach((k,v) -> System.out.println(k + ": " + v));
+				for (HashMap.Entry<?, ?> data : operations[i].getDetails().entrySet()) {
+		            System.out.println(data.getKey() + ": " + data.getValue());
+				}
+				
+				// if (type == "onboarding") { ... }
+				// if (type == "cashin") { ... }
+				// if (type == "cashout") { ... }
+				// if (type == "closing") { ... }
+				
 				System.out.println();
 			}
 			
-			reader.close();
+			opsReader.close();
 			
 		} 
 		
 		catch (FileNotFoundException e) { e.printStackTrace(); } 
 		catch (IOException e) {	e.printStackTrace(); }
-	
-		System.out.println("Test");
 		
 	}
 
